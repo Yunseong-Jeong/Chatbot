1ab3893ae4ebcb789e22bfee970439b2b7a2482b
Yunseong Jeong <yunseong14@naver.com>
2019-11-18 09:25:35 +0000

    Update

--- chatbot.js
+++ chatbot.js
@@ -7,13 +7,25 @@ const token = process.env.SLACK_TOKEN;
 const rtm = new RTMClient(token);
 rtm.start();
 
+const food = require('./food');
+const movie = require('./movie');
+
+
 rtm.on('message', (message) => {
   const { channel } = message;
   const { text } = message;
 
-  if (text === '안녕') {
-    rtm.sendMessage('헬로', channel);
-  } else {
-    rtm.sendMessage('응?', channel);
+  switch (text) {
+    case '영화':
+      movie(rtm, channel);
+      break;
+    case '밥':
+      food(rtm, channel);
+      break;
+    case '놀이':
+      rtm.sendMessage('고만해', channel);
+      break;
+    default:
+      rtm.sendMessage('안녕하세요. 영화, 밥, 중에 말씀해주세요.', channel);
   }
 });
--- test.spec.js
+++ test.spec.js
@@ -2,15 +2,26 @@ require('dotenv').config();
 
 const token = process.env.SLACK_TOKEN;
 const tchannel = process.env.TESTING_CHANNEL;
-const { RTMClient, LogLevel} = require('@slack/rtm-api');
+const { RTMClient, LogLevel } = require('@slack/rtm-api');
 
 const rtm = new RTMClient(token, {});
 
 
 (async () => {
-                await rtm.start()
-                    .catch(console.error);
-                    })();
+  await rtm.start()
+    .catch(console.error);
+})();
 
 
-const
+const assert = require('assert');
+const food = require('./food');
+const movie = require('./movie');
+
+let res;
+describe('유닛 테스트를 시작합니다.', async () => {
+  before(async () => res = await food(rtm, tchannel));
+  it('밥 메시지 테스트', (done) => {
+    assert.equal(res, 'success');
+    done();
+  });
+});
